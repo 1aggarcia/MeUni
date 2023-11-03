ebe0198b05ef697faa59a4a8199b82ecd19bc674
Sandeep Singh <sandeep01@hotmail.sg>
2023-11-03 06:49:18 -0700

    Refactor API

--- api/bin/server.dart
+++ api/bin/server.dart
@@ -4,45 +4,25 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-import 'article.dart';
-
-List<Article> articles = [];
-
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/articles', _getArticlesHandler)
-  ..post('/articles', _postArticleHandler);
+import 'controllers/article_controller.dart';
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!\n');
 }
 
 void main(List<String> args) async {
+  // Configure routes.
+  var router = Router()..get('/', _rootHandler);
+  router = ArticleController().setUpRoutes(router, '/articles');
+
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(router);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
-}
-
-Response _getArticlesHandler(Request request) {
-  return Response.ok(articlesToJson(articles));
-}
-
-Future<Response> _postArticleHandler(Request request) async {
-  String body = await request.readAsString();
-
-  try {
-    Article article = articleFromJson(body);
-    articles.add(article);
-    return Response.ok(articleToJson(article));
-  } catch (e) {
-    return Response(400);
-  }
 }
