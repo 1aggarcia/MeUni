3b921e9dd6be5ae02c2ab1afe14fcd2ec39882df
Sandeep Singh <sandeep01@hotmail.sg>
2023-11-17 10:26:13 -0800

    [mobile] Create Profile View

--- mobile/lib/ui/views/create_profile/create_profile_view.dart
+++ mobile/lib/ui/views/create_profile/create_profile_view.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:meuni_mobile/ui/common/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked/stacked_annotations.dart';
 
+import '../create_event/text_input.dart';
+import 'create_profile_view.form.dart';
 import 'create_profile_viewmodel.dart';
 
-class CreateProfileView extends StackedView<CreateProfileViewModel> {
+@FormView(fields: [
+  FormTextField(name: 'firstName'),
+  FormTextField(name: 'lastName'),
+  FormTextField(
+    name: 'year',
+    validator: CreateProfileValidators.validateYear,
+  ),
+  FormTextField(name: 'pronouns'),
+])
+class CreateProfileView extends StackedView<CreateProfileViewModel>
+    with $CreateProfileView {
   //* Private Properties
   final String userId;
 
@@ -13,15 +27,124 @@ class CreateProfileView extends StackedView<CreateProfileViewModel> {
     required this.userId,
   }) : super(key: key);
 
-  //* Overriden Methods
+  //* Overridden Methods
   @override
   Widget builder(
       BuildContext context, CreateProfileViewModel viewModel, Widget? child) {
+    const labelTextStyle = TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w700,
+    );
+
+    const errorTextStyle = TextStyle(
+      color: Colors.red,
+      fontSize: 12,
+      fontWeight: FontWeight.w700,
+    );
+
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-        child: Text(userId),
+      body: SafeArea(
+        child: viewModel.isBusy
+            ? const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Creating Profile ...",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    verticalSpaceMedium,
+                    CircularProgressIndicator(),
+                  ],
+                ),
+              )
+            : SingleChildScrollView(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Let's get you set up!",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    verticalSpaceLarge,
+
+                    // Adding a profile picture
+                    GestureDetector(
+                      child: CircleAvatar(
+                        radius: 60,
+                        backgroundColor: Colors.grey[300],
+                        child: const Icon(
+                          Icons.add_a_photo,
+                          size: 50,
+                        ),
+                      ),
+                    ),
+
+                    verticalSpaceLarge,
+
+                    // First Name
+                    TextFormField(
+                      controller: firstNameController,
+                      decoration: const InputDecoration(labelText: 'Last name'),
+                    ),
+                    Text(firstNameController.text),
+
+                    verticalSpaceSmall,
+
+                    // Last Name
+                    TextFormField(
+                      controller: lastNameController,
+                      decoration: const InputDecoration(labelText: 'Last name'),
+                    ),
+
+                    verticalSpaceSmall,
+
+                    // Year
+                    TextFormField(
+                      controller: yearController,
+                      // keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(labelText: 'Year'),
+                    ),
+                    if (viewModel.hasYearValidationMessage) ...[
+                      verticalSpaceTiny,
+                      Text(
+                        viewModel.yearValidationMessage!,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+
+                    verticalSpaceSmall,
+
+                    // Pronouns
+                    TextFormField(
+                      controller: pronounsController,
+                      decoration: const InputDecoration(labelText: 'Pronouns'),
+                    ),
+
+                    verticalSpaceMedium,
+
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                      ),
+                      onPressed: () async =>
+                          await viewModel.createProfileAsync(),
+                      child: const Text('Create Profile'),
+                    ),
+                  ],
+                ),
+              ),
       ),
     );
   }
@@ -29,4 +152,15 @@ class CreateProfileView extends StackedView<CreateProfileViewModel> {
   @override
   CreateProfileViewModel viewModelBuilder(BuildContext context) =>
       CreateProfileViewModel(userId: userId);
+
+  @override
+  void onViewModelReady(CreateProfileViewModel viewModel) {
+    syncFormWithViewModel(viewModel);
+  }
+
+  @override
+  void onDispose(CreateProfileViewModel viewModel) {
+    super.onDispose(viewModel);
+    disposeForm();
+  }
 }
--- mobile/lib/ui/views/create_profile/create_profile_viewmodel.dart
+++ mobile/lib/ui/views/create_profile/create_profile_viewmodel.dart
@@ -1,9 +1,58 @@
+import 'package:meuni_mobile/app/app.router.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class CreateProfileViewModel extends BaseViewModel {
+import '../../../app/app.locator.dart';
+import '../../../models/user.dart';
+import '../../../repository/users_repo.dart';
+import 'create_profile_view.form.dart';
+
+class CreateProfileViewModel extends FormViewModel {
   //* Properties
+  final _usersRepo = locator<UsersRepo>();
+  final _navigationService = locator<NavigationService>();
+
   final String userId;
 
   //* Constructors
   CreateProfileViewModel({required this.userId});
+
+  //* Public Methods
+  Future createProfileAsync() async {
+    if (isFormValid) {
+      setBusy(true);
+
+      User user = await _usersRepo.addUserAsync(
+        id: userId,
+        firstName: firstNameValue!,
+        lastName: lastNameValue!,
+        year: int.parse(yearValue!),
+        pronouns: pronounsValue!,
+      );
+
+      _usersRepo.loggedInUser = user;
+
+      setBusy(false);
+
+      _navigationService.replaceWithEventsView();
+    }
+  }
+}
+
+class CreateProfileValidators {
+  static String? validateYear(String? value) {
+    if (value == null) {
+      return null;
+    }
+
+    int? year = int.tryParse(value);
+
+    if (year == null) {
+      return 'Only numbers allowed';
+    } else if (year <= 0 || year > 4) {
+      return 'Year must be from 1 to 4.';
+    }
+
+    return null;
+  }
 }
