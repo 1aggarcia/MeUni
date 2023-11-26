4c9971c590843ffa431e035b1092985c26b66edc
Sandeep Singh <sandeep01@hotmail.sg>
2023-11-25 16:24:13 -0800

    [mobile] Implement CreateIEventsView

--- mobile/lib/ui/views/create_event/create_event_view.dart
+++ mobile/lib/ui/views/create_event/create_event_view.dart
@@ -1,202 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-import 'package:stacked/stacked_annotations.dart';
 
-import '../../common/app_colors.dart';
-import '../../common/ui_helpers.dart';
-import '../../widgets/loading_indicator.dart';
-import 'create_event_view.form.dart';
+import '../../../models/event.dart';
+import '../../widgets/i_event/create_i_event_view/create_i_event_view.dart';
 import 'create_event_viewmodel.dart';
 
-@FormView(fields: [
-  FormTextField(
-    name: 'eventName',
-    validator: CreateEventValidators.validateEventName,
-  ),
-  FormTextField(
-    name: 'eventDescription',
-    validator: CreateEventValidators.validateEventDescription,
-  ),
-  FormTextField(
-    name: 'eventLocation',
-    validator: CreateEventValidators.validateEventLocation,
-  ),
-  FormTextField(
-    name: 'eventDate',
-    validator: CreateEventValidators.validateEventDate,
-  ),
-  FormTextField(
-    name: 'eventStartTime',
-    validator: CreateEventValidators.validateEventStartTime,
-  ),
-  FormTextField(
-    name: 'eventEndTime',
-    validator: CreateEventValidators.validateEventEndTime,
-  ),
-])
-class CreateEventView extends StackedView<CreateEventViewModel>
-    with $CreateEventView {
+class CreateEventView extends StackedView<CreateEventViewModel> {
   const CreateEventView({super.key});
 
-  //* Overriden Methods
+  //* Overridden Methods
   @override
   CreateEventViewModel viewModelBuilder(BuildContext context) =>
       CreateEventViewModel();
 
   @override
-  void onViewModelReady(CreateEventViewModel viewModel) =>
-      syncFormWithViewModel(viewModel);
-
-  @override
-  void onDispose(CreateEventViewModel viewModel) {
-    super.onDispose(viewModel);
-    disposeForm();
-  }
-
-  @override
   Widget builder(
       BuildContext context, CreateEventViewModel viewModel, Widget? child) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Create Event'),
-          backgroundColor: kcPrimaryColor,
-          foregroundColor: kcSecondaryColor,
-        ),
-        backgroundColor: kcBackgroundColor,
-        body: viewModel.isBusy
-            ? const LoadingIndicator(loadingText: 'Creating Event')
-            : Container(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Event Name
-                      TextFormField(
-                        controller: eventNameController,
-                        decoration: InputDecoration(
-                          labelText: 'Event Name',
-                          errorText: viewModel.eventNameValidationMessage,
-                        ),
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Event Description
-                      TextFormField(
-                        controller: eventDescriptionController,
-                        decoration: InputDecoration(
-                          labelText: 'Event Description',
-                          errorText:
-                              viewModel.eventDescriptionValidationMessage,
-                        ),
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Event Location
-                      TextFormField(
-                        controller: eventLocationController,
-                        decoration: InputDecoration(
-                          labelText: 'Event Location',
-                          errorText: viewModel.eventLocationValidationMessage,
-                        ),
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Event Date
-                      TextFormField(
-                        controller: eventDateController,
-                        decoration: InputDecoration(
-                          labelText: 'Event Date',
-                          errorText: viewModel.eventDateValidationMessage,
-                        ),
-                        onTap: () async {
-                          DateTime? picked = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime.now(),
-                            lastDate: DateTime.now().add(
-                              const Duration(days: 365),
-                            ),
-                          );
-                          if (picked != null) {
-                            eventDateController.text = picked.toString();
-                          }
-                        },
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Event Start Time
-                      TextFormField(
-                        controller: eventStartTimeController,
-                        decoration: InputDecoration(
-                            labelText: 'Event Start Time',
-                            errorText:
-                                viewModel.eventStartTimeValidationMessage),
-                        onTap: () async {
-                          TimeOfDay time = TimeOfDay.now();
-                          FocusScope.of(context).requestFocus(FocusNode());
-
-                          TimeOfDay? picked = await showTimePicker(
-                            context: context,
-                            initialTime: time,
-                          );
-                          if (picked != null && picked != time) {
-                            eventStartTimeController.text =
-                                // ignore: use_build_context_synchronously
-                                picked.format(context);
-                          }
-                        },
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Event End Time
-                      TextFormField(
-                        controller: eventEndTimeController,
-                        decoration: InputDecoration(
-                          labelText: 'Event End Time',
-                          errorText: viewModel.eventEndTimeValidationMessage,
-                        ),
-                        onTap: () async {
-                          TimeOfDay time = TimeOfDay.now();
-                          FocusScope.of(context).requestFocus(FocusNode());
-
-                          TimeOfDay? picked = await showTimePicker(
-                            context: context,
-                            initialTime: time,
-                          );
-                          if (picked != null && picked != time) {
-                            eventEndTimeController.text =
-                                // ignore: use_build_context_synchronously
-                                picked.format(context);
-                          }
-                        },
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Add Event Button
-                      MaterialButton(
-                        color: kcPrimaryColor,
-                        disabledColor: kcPrimaryColor.withAlpha(100),
-                        onPressed: viewModel.isFormValid
-                            ? () async => await viewModel.addEventAsync()
-                            : null,
-                        child: const Text(
-                          'Add Event',
-                          style: TextStyle(color: kcSecondaryColor),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-      ),
-    );
+    return CreateIEventView<Event>();
   }
 }
--- mobile/lib/ui/views/create_study_group/create_study_group_view.dart
+++ mobile/lib/ui/views/create_study_group/create_study_group_view.dart
@@ -1,199 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-import 'package:stacked/stacked_annotations.dart';
 
-import '../../common/app_colors.dart';
-import '../../common/ui_helpers.dart';
-import 'create_study_group_view.form.dart';
+import '../../../models/study_group.dart';
+import '../../widgets/i_event/create_i_event_view/create_i_event_view.dart';
 import 'create_study_group_viewmodel.dart';
 
-@FormView(fields: [
-  FormTextField(
-    name: 'studyGroupCourse',
-    validator: CreateStudyGroupValidators.validateStudyGroupCourse,
-  ),
-  FormTextField(
-    name: 'studyGroupDescription',
-    validator: CreateStudyGroupValidators.validateStudyGroupDescription,
-  ),
-  FormTextField(
-    name: 'studyGroupLocation',
-    validator: CreateStudyGroupValidators.validateStudyGroupLocation,
-  ),
-  FormTextField(
-    name: 'studyGroupDate',
-    validator: CreateStudyGroupValidators.validateStudyGroupDate,
-  ),
-  FormTextField(
-    name: 'studyGroupStartTime',
-    validator: CreateStudyGroupValidators.validateStudyGroupStartTime,
-  ),
-  FormTextField(
-    name: 'studyGroupEndTime',
-    validator: CreateStudyGroupValidators.validateStudyGroupEndTime,
-  ),
-])
-class CreateStudyGroupView extends StackedView<CreateStudyGroupViewModel>
-    with $CreateStudyGroupView {
+class CreateStudyGroupView extends StackedView<CreateStudyGroupViewModel> {
   const CreateStudyGroupView({super.key});
 
-  @override
-  Widget builder(BuildContext context, CreateStudyGroupViewModel viewModel,
-      Widget? child) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Create Study Group')),
-        body: viewModel.isLoading
-            ? const Text('Loading...')
-            : Container(
-                padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      verticalSpaceMedium,
-
-                      // StudyGroup Course Name
-                      TextFormField(
-                        controller: studyGroupCourseController,
-                        decoration: InputDecoration(
-                          labelText: 'Study Group Course Name',
-                          errorText:
-                              viewModel.studyGroupCourseValidationMessage,
-                        ),
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Study Group Description
-                      TextFormField(
-                        controller: studyGroupDescriptionController,
-                        decoration: InputDecoration(
-                          labelText: 'Study Group Description',
-                          errorText:
-                              viewModel.studyGroupDescriptionValidationMessage,
-                        ),
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Study Group Location
-                      TextFormField(
-                        controller: studyGroupLocationController,
-                        decoration: InputDecoration(
-                          labelText: 'Study Group Location',
-                          errorText:
-                              viewModel.studyGroupLocationValidationMessage,
-                        ),
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Study Group Date
-                      TextFormField(
-                        controller: studyGroupDateController,
-                        decoration: InputDecoration(
-                          labelText: 'Study Group Date',
-                          errorText: viewModel.studyGroupDateValidationMessage,
-                        ),
-                        onTap: () async {
-                          DateTime? picked = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime.now(),
-                            lastDate:
-                                DateTime.now().add(const Duration(days: 365)),
-                          );
-                          if (picked != null) {
-                            studyGroupDateController.text = picked.toString();
-                          }
-                        },
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Study Group Start Time
-                      TextFormField(
-                        controller: studyGroupStartTimeController,
-                        decoration: InputDecoration(
-                          labelText: 'Study Group Start Time',
-                          errorText:
-                              viewModel.studyGroupStartTimeValidationMessage,
-                        ),
-                        onTap: () async {
-                          TimeOfDay time = TimeOfDay.now();
-                          FocusScope.of(context).requestFocus(FocusNode());
-
-                          TimeOfDay? picked = await showTimePicker(
-                            context: context,
-                            initialTime: time,
-                          );
-                          if (picked != null && picked != time) {
-                            studyGroupStartTimeController.text =
-                                // ignore: use_build_context_synchronously
-                                picked.format(context);
-                          }
-                        },
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Study Group End Time
-                      TextFormField(
-                        controller: studyGroupEndTimeController,
-                        decoration: InputDecoration(
-                          labelText: 'Study Group End Time',
-                          errorText:
-                              viewModel.studyGroupEndTimeValidationMessage,
-                        ),
-                        onTap: () async {
-                          TimeOfDay time = TimeOfDay.now();
-                          FocusScope.of(context).requestFocus(FocusNode());
-
-                          TimeOfDay? picked = await showTimePicker(
-                            context: context,
-                            initialTime: time,
-                          );
-                          if (picked != null && picked != time) {
-                            studyGroupEndTimeController.text =
-                                // ignore: use_build_context_synchronously
-                                picked.format(context);
-                          }
-                        },
-                      ),
-
-                      verticalSpaceSmall,
-
-                      // Add Study Group Button
-                      MaterialButton(
-                        color: kcTextPrimaryColor,
-                        onPressed: () async =>
-                            await viewModel.addStudyGroupAsync(),
-                        child: const Text(
-                          'Add Study Group',
-                          style: TextStyle(color: kcSecondaryColor),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-      ),
-    );
-  }
-
-  //* Overriden Methods
+  //* Overridden Methods
   @override
   CreateStudyGroupViewModel viewModelBuilder(BuildContext context) =>
       CreateStudyGroupViewModel();
 
   @override
-  void onViewModelReady(CreateStudyGroupViewModel viewModel) =>
-      syncFormWithViewModel(viewModel);
-
-  @override
-  void onDispose(CreateStudyGroupViewModel viewModel) {
-    super.onDispose(viewModel);
-    disposeForm();
+  Widget builder(BuildContext context, CreateStudyGroupViewModel viewModel,
+      Widget? child) {
+    return CreateIEventView<StudyGroup>();
   }
 }
