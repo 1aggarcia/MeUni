6197e2842cf4fbec6762b544b90cfb5456c098c9
jintaecho <ber1872@cs.washington.edu>
2023-11-26 17:45:11 -0800

    update on getUser

--- api/bin/models/user.dart
+++ api/bin/models/user.dart
@@ -1,71 +1,63 @@
 import 'dart:convert';
 
-// User userFromJson(String str, int userId) {
-//   var decode = json.decode(str);
-//   return User.fromJson(decode, userId);
-// }
-
-String userToJson(User data) => json.encode(data.toJson());
-
-User? userFromJson(String str) {
-  try {
-    return User.fromJsonMap(json.decode(str));
-  } catch (e) {
-    return null;
-  }
-}
-
-
-String usersToJson(List<User> data) =>
-    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
-
 class User {
-  User({
-    required this.id,
-    required this.firstName,
-    required this.lastName,
-    required this.year,
-    required this.pronouns,
-    required this.admin
-  });
-
-  final String id;
+  String? id;
   final String firstName;
   final String lastName;
   final int year;
   final String pronouns;
   final bool admin;
 
-  factory User.fromJson(Map<String, dynamic> json, int userId) => User(
-        id: json['id'],
+  User(
+      {this.id,
+      required this.firstName,
+      required this.lastName,
+      required this.year,
+      required this.pronouns,
+      required this.admin});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    if (json['id'] is! String ||
+        json['firstName'] is! String ||
+        json['lastName'] is! String ||
+        json['year'] is! String ||
+        json['pronouns'] is! String ||
+        json['admin'] is! String) {
+      throw Exception(
+          "json map passed in with incorrect or missing params for User:\n$json");
+    }
+
+    return User(
         firstName: json['firstName'],
         lastName: json['lastName'],
         year: json['year'],
         pronouns: json['pronouns'],
-        admin: json['admin'],
-    );
-
-  Map<String, User> fromJsonMap(Map<String, dynamic> data) {
-    Map<String, User> users = {};
-    data.forEach((k, v) {
-      if (int.tryParse(k) != null) {
-        try {
-          User e = User.fromJson(v);
-          users[k] = e;
-        } catch (e) {
-          print ("fromJsonMap: $e");
-        }
-      }
-    });
-    return users;
+        admin: json['admin']);
   }
 
+  /// Returns a json map of User instance
   Map<String, dynamic> toJson() => {
-        'id' : id,
-        'firstName' : firstName,
+        'id': id,
+        'firstName': firstName,
         'lastName': lastName,
         'year': year,
-        'pronouns' : pronouns,
-        'admin' : admin,
+        'pronouns': pronouns,
+        'admin': admin,
       };
 }
+
+User? userFromJson(String str) {
+  try {
+    return userFromMap(json.decode(str));
+  } catch (e) {
+    print("ERROR: userFromJson() $e");
+    return null;
+  }
+}
+
+User? userFromMap(Map<String, dynamic> data) {}
+
+String userToJson(User data) => json.encode(data.toJson());
+
+String usersToJson(List<User> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
--- api/bin/repository/users_repo.dart
+++ api/bin/repository/users_repo.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
 
 import '../models/user.dart';
 
@@ -8,28 +7,11 @@ import 'package:firebase_dart/database.dart' as db;
 abstract class UsersRepo {
   //* Public Methods
 
-  // /// @returns the ID of the newly created user account
-  // Future<int> createUserAsync(User user);
-
-  // /// Replace the existing set of user information with
-  // /// the given set of user information
-  // /// @returns the updated set of uesr information
-  // Future<List<String>?> updateUserAsync(String userId, String firstName, String lastName, String year, String pronouns);
-
   /// Returns the User if found, null otherwise
   Future<User?> getUserAsync(String id);
-  
 }
 
 class UsersRepoImpl extends UsersRepo {
-  // TODO : get database reference with locator (example in events_repo.dart)
-
-  //* Overriden Methods
-  // @override
-  // Future<int> createUserAsync(User user) async {
-
-  //   retur
-  // }
   late db.DatabaseReference _userRef;
 
   @override
@@ -38,20 +20,4 @@ class UsersRepoImpl extends UsersRepo {
     final json = jsonEncode(snapshot.value);
     return userFromJson(json);
   }
-
-  // @override
-  // Future<List<String>?> updateUserAsync(String userId, String firstName, String lastName, String year, String pronouns) {
-  //   User? user = await getUserAsync(userID) as User;
-  //   user.
-  // }
-
-  // /// Finds an available id in the database
-  // /// @returns new id and its DatabaseReference
-  // Future<(int, db.DatabaseReference)> getNewRefAsync() async {
-  //   final rand = Random();
-  //   int id = rand.nextInt(maxUsers);
-  //   List<int> used = [id];
-  //   db.DatabaseReference newRef = _usersRef.child("$id");
-  //   db.DataSnapshot snapshot = await newRef.once();
-  // }
 }
